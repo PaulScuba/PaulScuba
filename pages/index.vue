dc2898bf0820333b2006d329e13091480edf5f0a
PaulScuba <paul.vandyk91@gmail.com>
2023-01-22 13:17:04 -0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <img src="../maps/campaign_map.jpg"/>      
+  </div>
+    <!-- <v-card>
+      <h1>The Dragon Spur</h1>
+    </v-card>
+    <v-card>
+      <img src="../maps/campaign_map.jpg"/>      
+    </v-card>  
+     -->
+  </template>
